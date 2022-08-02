9a1b30fc243630eea97a1e04c9512b3af4d1f853
hyeonju-j <fermata52321@gmail.com>
2022-08-02 17:11:06 +0900

    share_board & admin_board front design

--- BookHa/src/main/java/com/bookha/controller/BoardTestController.java
+++ BookHa/src/main/java/com/bookha/controller/BoardTestController.java
@@ -33,4 +33,69 @@ public class BoardTestController {
 		mv.setViewName("review_board/board_view2");
 		return mv;
 	}
+	
+	@RequestMapping(value = "/share_list.do")
+	public ModelAndView shareList(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new ModelAndView();
+		//mv.addObject("msg", "get");
+		mv.setViewName("share_board/board_list1");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/share_write.do")
+	public ModelAndView shareWrite(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new ModelAndView();
+		//mv.addObject("msg", "get");
+		mv.setViewName("share_board/board_write1");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/share_view.do")
+	public ModelAndView shareView(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new ModelAndView();
+		//mv.addObject("msg", "get");
+		mv.setViewName("share_board/board_view1");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/admin_main.do")
+	public ModelAndView adminMain(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new ModelAndView();
+		//mv.addObject("msg", "get");
+		mv.setViewName("admin_board/board_main1");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/admin_list.do")
+	public ModelAndView adminList(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new ModelAndView();
+		//mv.addObject("msg", "get");
+		mv.setViewName("admin_board/board_list1");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/admin_write.do")
+	public ModelAndView adminWrite(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new ModelAndView();
+		//mv.addObject("msg", "get");
+		mv.setViewName("admin_board/board_write1");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/admin_view.do")
+	public ModelAndView adminView(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new ModelAndView();
+		//mv.addObject("msg", "get");
+		mv.setViewName("admin_board/board_view1");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/notice_view.do")
+	public ModelAndView noticeView(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new ModelAndView();
+		//mv.addObject("msg", "get");
+		mv.setViewName("admin_board/board_notice1");
+		return mv;
+	}
+	
 }
